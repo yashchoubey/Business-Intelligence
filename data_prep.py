09ac4e9b50a2fc1d3e291a7a31399189858458d2
yashchoubey <yashchoubey2009@gmail.com>
2018-09-06 00:30:14 +0530

    dataprep

--- data_prep.py
+++ data_prep.py
@@ -22,10 +22,12 @@
 # classifier.fit_generator(training_set,steps_per_epoch = 8000,epochs = 25,validation_data = test_set,validation_steps = 2000)
 
 image_list=[]
+label_list=[]
 import os,cv2,numpy as np
 par_dir='/home/yash/github/t_cLASSIFICTION/Layer/layer1/'
 train_labels = os.listdir(par_dir)
 
+label_dict={'others':2, 'ruptured_skin':0, 'deformed_double':1}
 # print(train_labels)
 for label in train_labels:
 	print par_dir+label
@@ -37,12 +39,19 @@ for label in train_labels:
 		# print image.shape
 		image = cv2.resize(image, (200,200))
 		image_list.append(image)
+		label_list.append(label_dict[label])
 		# print par_dir+label+image
 		# break
-print len(image_list)
+# print len(image_list)
 a=np.asarray(image_list)
 a=a.reshape(27587,120000)
 del image_list
 from tempfile import TemporaryFile
 outfile = TemporaryFile()
-np.save('tomato', a)
+np.save('images_level1', a)
+
+# print set(label_list)
+import keras
+label_list=keras.utils.to_categorical(label_list, num_classes=3)
+# print len(label_list),type(label_list)
+np.save('labels_level1', label_list)
