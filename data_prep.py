b1c917b8c8dbee116a1775e80f088996bed9d72c
yashchoubey <yashchoubey2009@gmail.com>
2018-09-10 23:21:16 +0530

    caffe data prep

--- data_prep.py
+++ data_prep.py
@@ -10,7 +10,7 @@ for label in train_labels:
 	print par_dir+label
 	images=os.listdir(par_dir+label)
 	print len(images)
-	for image in images:
+	for image in images:#[:30]:
 		file= par_dir+label+'/'+image
 		image = cv2.imread(file)
 		# print image.shape
@@ -20,6 +20,37 @@ for label in train_labels:
 		# print par_dir+label+image
 		# break
 
+image_list = np.asarray(image_list).transpose(0,3,1,2)
+label_list = np.asarray(label_list)[None,None,None].transpose(3,0,1,2)
+
+print image_list.shape
+print label_list.shape
+
+from sklearn.model_selection import train_test_split
+image_list, X_test, label_list, y_test = train_test_split(image_list, label_list, test_size=0, random_state=4)
+
+
+import h5py
+
+hdf_file = h5py.File('train.h5', 'w')
+hdf_file.create_dataset('data', dtype=np.float, data=image_list[:-5518,:,:,:], chunks=True)
+hdf_file.create_dataset('label', dtype=np.float, data=label_list[:-5518,:,:,:])
+hdf_file = h5py.File('test.h5', 'w')
+hdf_file.create_dataset('data', dtype=np.float, data=image_list[-5518:,:,:,:], chunks=True)
+hdf_file.create_dataset('label', dtype=np.float, data=label_list[-5518:,:,:,:])
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 # print len(image_list)
 a=np.asarray(image_list)
 
@@ -45,3 +76,4 @@ import keras
 label_list=keras.utils.to_categorical(label_list, num_classes=3)
 # print len(label_list),type(label_list)
 np.save('labels_level1', label_list)
+'''
